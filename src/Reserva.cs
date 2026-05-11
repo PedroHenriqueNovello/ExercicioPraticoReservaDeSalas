c760d2a9e068ef6bdcc09f2c8c99545d08723eac
Letícia <moreira.leticia@unifesp.br>
2026-05-11 16:32:19 -0300

    feat: Atualiza classe Reserva com atributos de Sala e DataHora.

--- src/Reserva.cs
+++ src/Reserva.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace ReservaDeSalas
 {
     public class Reserva 
     { 
         public string Id { get; set; }
         public string Detalhes { get; set; } 
+        public Sala Sala { get; set; }
+        public DateTime DataHora { get; set; }
     }
 }
