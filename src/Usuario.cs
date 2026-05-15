85e497055bb8a724edbc4720c4b2fa78f5999970
Enrico Bertoni <em.bertoni@unifesp.br>
2026-05-15 00:11:01 -0300

    Nível de acesso adicionado na classe Usuario

--- src/Usuario.cs
+++ src/Usuario.cs
@@ -6,17 +6,17 @@ namespace ReservaDeSalas
     {
         public string Nome { get; private set; }
         public bool IsDocente { get; set; }
+        public NivelAcesso Nivel { get; set; }
 
-        public Usuario(string nome) => Nome = nome;
+        public Usuario(string nome)
+        {
+            Nome = nome;
+            Nivel = NivelAcesso.Aluno;
+        }
 
         public void Update(ISubject subject, Reserva reserva)
         {
-            // PUSH
             Console.WriteLine($"Para {Nome}: A reserva '{reserva.Id}' na sala tipo '{reserva.Sala.GetTipo()}' foi atualizada.");
-
-            // PULL
-            int total = subject.GetTotalReservasAtivas();
-            Console.WriteLine($"O sistema agora possui {total} reserva(s) no total.");
         }
     }
 }
